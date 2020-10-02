8ea58bc2fcd59339a0af5915dcadbfa5f5ad7f8d
Maxime Souillat <maxime@beaucouplus.com>
2020-10-02 17:44:34 +0200

    step 2 > created form view, form object and updated controller actions

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -6,6 +6,20 @@ class QuestionnairesController < ApplicationController
   end
 
   def show
-    # do something
+    @questionnaire = QuestionnaireForm.new
+  end
+
+  def create
+    @questionnaire = QuestionnaireForm.new({ token: session[:questionnaire_token] }.merge(questionnaire_params))
+
+    if @questionnaire.save
+      redirect_to score_path(id: session[:questionnaire_token])
+    else
+      render :show
+    end
+  end
+
+  def questionnaire_params
+    params.require(:questionnaire_form).permit(answers: {})
   end
 end
---
+++ app/lib/questionnaire_form.rb
@@ -0,0 +1,31 @@
+class QuestionnaireForm
+  include ActiveModel::Model
+
+  PAIRS = [
+    %w[AMBITION THOROUGHNESS],
+    %w[AMBITION PATIENCE],
+    %w[EMPATHY THOROUGHNESS],
+    %w[AMBITION EMPATHY],
+    %w[THOROUGHNESS PATIENCE],
+    %w[EMPATHY PATIENCE]
+  ].freeze
+
+  attr_accessor :token, :answers
+  def questions
+    PAIRS
+  end
+
+  def save
+    answers.each do |key, answer|
+      Answer.create(chosen_dimension: answer,
+                    other_dimension: other_dimension(key, answer),
+                    intensity: 1,
+                    questionnaire_token: token)
+    end
+    true
+  end
+
+  def other_dimension(key, answer)
+    PAIRS[key.to_i].find { |dimension| dimension != answer }
+  end
+end

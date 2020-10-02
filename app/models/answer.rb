98a2717685280cfa9c149d7a5971dab7c05c0e50
Maxime Souillat <maxime@beaucouplus.com>
2020-10-02 11:45:35 +0200

    step 1 > implemented algorithm > tests green

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,21 @@
 class Answer < ApplicationRecord
+  INTENSITY_TO_SCORE = { 1 => 1, 2 => 3 }.freeze
+
   def self.scores_for(questionnaire_token:)
-    { 'AMBITION': 0,
-      'THOROUGHNESS': 0 }
+    answers = where(questionnaire_token: questionnaire_token).to_a
+
+    scores = {
+      'AMBITION' => 0,
+      'THOROUGHNESS' => 0,
+      'PATIENCE' => 0,
+      'EMPATHY' => 0
+    }
+    answers.each do |answer|
+      score_value = INTENSITY_TO_SCORE[answer.intensity]
+      scores[answer.chosen_dimension] += score_value
+      scores[answer.other_dimension] -= score_value
+      scores[answer.other_dimension] = 0 if scores[answer.other_dimension].negative?
+    end
+    scores
   end
 end

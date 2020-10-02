b44059ebef17589619d385871ca5b0fa5e450661
Maxime Souillat <maxime@beaucouplus.com>
2020-10-02 12:34:59 +0200

    step 2 > created action index

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,6 +1,8 @@
 class QuestionnairesController < ApplicationController
   def index
-    # do something
+    token = SecureRandom.base58
+    session[:questionnaire_token] = token
+    redirect_to questionnaire_path(id: token)
   end
 
   def show
